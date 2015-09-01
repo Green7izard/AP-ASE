cecab09ce95005e00abe72e1bb5595e76319dd47
Bas van Summeren <basvanummeren@hotmail.com>
2015-09-01 21:11:20 +0200

    Finished day 3 of RUBY

--- Ruby/day3.rb
+++ Ruby/day3.rb
@@ -1,6 +1,29 @@
 # Assignment Ruby day 3
 # Bas van Summeren<BasVanSummeren@hotmail.com>
 
+class CsvRow
+  attr_accessor :values
+
+  def initialize(headers, values)
+    i=0;
+    @values ={}
+    headers.each{|head|
+      @values[head]=values[i]
+      i+=1
+    }
+  end
+
+  def method_missing(name, *args)
+    temp = @values[name.to_s]
+    puts @values[name]
+    if(temp.nil?)
+      super.method_missing name, args
+    else
+      return temp
+    end
+  end
+
+end
 
 module ActsAsCsv
   def self.included(base)
@@ -18,7 +41,7 @@ module ActsAsCsv
       file = File.new(filename)
       @headers = file.gets.chomp.split(', ' )
       file.each do |row|
-        @csv_contents << row.chomp.split(', ' )
+        @csv_contents.push(CsvRow.new(@headers, row.chomp.split(', ' )))
       end
     end
     attr_accessor :headers, :csv_contents
@@ -30,7 +53,19 @@ end
 class Persoon # no inheritance! You can mix it in
   include ActsAsCsv
   acts_as_csv
+
+  def each(&block)
+    @csv_contents.each(&block)
+  end
+
 end
 m = Persoon.new
-puts m.headers.inspect
-puts m.csv_contents.inspect
+
+m.each{|row|
+  puts "Persoon"
+  puts "Naam: "+ row.naam
+  puts "Woonplaats: "+ row.woonplaats
+  puts "Leeftijd : "+row.leeftijd
+  puts "Geslacht : "+row.geslacht
+  puts "---------------------------"
+}
