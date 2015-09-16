89448160bce15b4a44d723333a48a52432e0fbac
Bas van Summeren <basvanummeren@hotmail.com>
2015-09-16 22:37:39 +0200

    Search is working

--- Haskell/src/Day2.hs
+++ Haskell/src/Day2.hs
@@ -1,7 +1,10 @@
 module Day2 where
 -- Sort Lists
+-- Need the sort function
+-- https://hackage.haskell.org/package/base-4.8.1.0/docs/Data-List.html
+import Data.List
 
-easySort :: (Ord a) =>[a] -> [a]
+easySort :: Ord a =>[a] -> [a]
 easySort list = sort list
 
 
@@ -10,11 +13,25 @@ easySort list = sort list
  -- In the following signature, we are setting the "constraint"
  -- that all values of "a" must be of an "orderable" type. This
  -- is because we are using a comprator.
-sortList :: (Ord a) => [a] -> (a -> a -> Ordering) -> [a]
-sortList condition list = sortBy condition list
+sortList :: Ord a => [a] -> (a -> a -> Ordering) -> [a]
+sortList list condition = sortBy condition list
+
+-- Sort by placing Evens first
+evenCompare :: Integer -> Integer -> Ordering
+evenCompare a b = if even a && not (even b)
+                then LT
+                 else if not (even a) && even b
+                  then GT
+                   else (compare a b)
+
+evenSort :: [Integer]-> [Integer]
+evenSort [] = []
+evenSort list = sortList list evenCompare
 
 -- Convert String to Number
 
+toString :: String -> Integer
+toString "" = 0
 
 -- Lazy list with paramter X where every third number starts with X. 
 -- Lazy list with paramter Y where every 5th number starts with y
