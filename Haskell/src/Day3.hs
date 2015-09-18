6afec853ba3209fe96777d47a6a2e5092772588b
Bas van Summeren <basvanummeren@hotmail.com>
2015-09-18 20:06:04 +0200

    Haskell day 3 map

--- Haskell/src/Day3.hs
+++ Haskell/src/Day3.hs
@@ -1,3 +1,21 @@
 module Day3 where
--- Monad time
+
+--Hash Map
+type KeyValue = (String, String)
+type Hash = [KeyValue]
+
+storeValue :: Hash->String->String->Hash
+storeValue map key value = storePair map (key, value)
+
+storePair :: Hash->KeyValue->Hash
+storePair map pair = map ++[pair]
+
+searchKey:: String -> Hash-> Maybe String
+searchKey key [] = Nothing
+searchKey [] map = Nothing
+searchKey key ((k, value):rest) =
+  if key == k  then Just value
+   else searchKey key rest
+
+exampleMap = [("Test",  "Value"), ("TheAnswer",  "42")]
 
