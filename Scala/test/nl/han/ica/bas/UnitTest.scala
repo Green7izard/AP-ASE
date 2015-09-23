f06f8efe8826d321f4c123ad94e13c96528ffa5a
Bas van Summeren <basvanummeren@hotmail.com>
2015-09-23 17:14:33 +0200

    Revert "Now using a diffrent base class for Unit Tests"
    This reverts commit 6f13e947d9b338bda020c86bc7fa1d0588fdb2f3.

--- Scala/test/nl/han/ica/bas/UnitTest.scala
+++ Scala/test/nl/han/ica/bas/UnitTest.scala
@@ -3,39 +3,44 @@ package nl.han.ica.bas
 /**
  * Created by Bas on 22-9-2015.
  * http://www.scalatest.org/quick_start
- * http://www.scalatest.org/getting_started_with_fun_suite
  */
 
 import org.scalatest._
 
+abstract class UnitTest extends FlatSpec with Matchers with OptionValues with Inside with Inspectors  {
+  "True" should "Be true" in{
+    true shouldBe(true);
+  }
+}
+
 import Fill._
-class BoardTest extends FunSuite{
+class BoardTest extends UnitTest{
   val board = new Board(2);
-  test("Board should have a Empty winner if new")  {
-    assert(board.getWinner() === Empty);
+  "Board" should "have Empty winner if created" in {
+    board.getWinner() shouldBe(Empty);
   }
-  test("Board should allow setting a value once") {
-    assert(board.setValue(0,0, Cross));
-  }
-
-  test("Board should not allow setting a value twice") {
-    assert(!board.setValue(0,0, Circle));
+  it should "Allow Setting a value once" in {
+    board.setValue(0,0, Cross) shouldBe(true);
   }
 
-  test("Board should have a Empty winner if new"){
-    assert(board.setValue(0,1, Circle));
-    assert(board.getWinner() ===Empty);
+  it should "Now Allow Setting a value twice" in {
+    board.setValue(0,0, Circle) shouldBe(false);
   }
-  test("Board should have Cross as a winner if it has 2 squares" ){
+
+  it should "Have no winner with a circle and a cross" in{
+    board.setValue(0,1, Circle) shouldBe(true);
+    board.getWinner() shouldBe(Empty);
+  }
+  it should "Have Cross as a winner if it has 2 squares" in{
     board setValue(0, 0, Cross);
     board setValue(1,1, Cross);
-    assert(board.getWinner() ===Cross);
+    board.getWinner() shouldBe(Cross);
   }
-  test( "Have Circle as a winner if it has 2 squares" ){
+  it should "Have Circle as a winner if it has 2 squares" in{
     board.reset();
     board setValue(0, 1, Circle);
     board setValue(1,0, Circle);
-   assert( board.getWinner() === Circle);
+    board.getWinner() shouldBe(Circle);
   }
 
 }
