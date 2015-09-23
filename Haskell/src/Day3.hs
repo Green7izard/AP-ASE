4f5357c2c4ee38d84ffa6f75c33cb520370d07e2
Bas van Summeren <basvanummeren@hotmail.com>
2015-09-23 13:50:46 +0200

    Extra comments for haskell day 3! And a extra solvable maze!

--- Haskell/src/Day3.hs
+++ Haskell/src/Day3.hs
@@ -46,65 +46,86 @@ type Node = ([(Int, Int)],Role)
 -- A maze is a collection of nodes in a two dimensional space
 type Maze = [[Node]]
 
+-- Gets a position, X is in the Main list (List of lists), Y is in the sublist (List of nodes)
 getNode ::Maze->(Int, Int)->Node
 getNode [] loc = ([(-1, -1)], Node)
 getNode maze (x, y) = (maze !! x)!!y
 
+-- Smaller maze for a simple demo
+smallMaze::Maze
 smallMaze = [
-               [([(0::Int,1::Int), (1,0)],Start), ([(1,1)],Node)],
+               [([(0,1), (1,0)],Start), ([(1,1)],Node)],
                [([],Node), ([],End)]
             ]
 
+-- Larger maze with more dead ends
 exampleMaze::Maze
 exampleMaze = [
-                 [([(0,1), (1,0)], Start), ([(2, 0)], Node),([(3,0)]       , Node)  ,([(3, 1)], Node)],
-                 [([(1,1)]       , Node) , ([(1, 2)], Node),([]            , End)   ,([], Node)],
-                 [([(0,3)]       , Node) , ([(0, 2)], Node),([(2,1)]       , Node)  ,([(3,1)], Node)],
-                 [([(1,3)]       , Node) , ([(2, 3)], Node),([(3,3), (2,2)], Node)  ,([(3,2)], Node)]
+                 [([(0,1), (1,0)], Start), ([(0, 2)], Node),([(0,3)]       , Node)  ,([(1, 3)], Node)],
+                 [([(1,1)]       , Node) , ([(2, 1)], Node),([]            , End)   ,([], Node)],
+                 [([(3,0)]       , Node) , ([(2, 0)], Node),([(1,2)]       , Node)  ,([(1,3)], Node)],
+                 [([(3,1)]       , Node) , ([(3, 2)], Node),([(3,3), (2,2)], Node)  ,([(2,3)], Node)]
               ]
+-- Maze that goes back and forth and circlelike and all
+loopMaze::Maze
+loopMaze = [
+                [([(0,1), (1,0)]     , Start), ([(0,0), (0,2)], Node),([(0,1), (0,3)], Node)         ,([(0,2), (1,3)], Node)],
+                [([(2,0), (0,0)]     , Node) , ([(0,1)       ], Node),([]            , End)          ,([(2,3), (0,3)], Node)],
+                [([(3,0), (1,0)]     , Node) , ([(1,1), (2,2)], Node),([(0,0), (2,1), (1,2)] ,Node) ,([(3,3), (1,3)], Node)],
+                [([(2,0), (3,1)]     , Node) , ([(3,0), (3,2)], Node),([(3,1), (3,3)], Node)         ,([(3,2), (2,3), (2,2)], Node)]
+           ]
 
 --Solving it
 --The solution is a path of nodes
 type Path = [Node]
 
-
+--Get the nodes that are cnnected to the given node
 getNextNodes::Maze->Node->[Node]
 getNextNodes [] nodes = []
 getNextNodes maze (exits, role)=[getNode maze x| x<-exits]
 
+--Find the start of the maze
 findStart :: [[Node]]->Node
 findStart list = head[node|subList<-list, node<-subList, isStart node]
 
+--Is this node the start?
 isStart :: Node->Bool
 isStart (list, role) = role==Start
 
+--Is this node the end?
 isEnd::Node->Bool
 isEnd (exits, role)= role==End
 
+--Has the path reached an end?
 hasEnd::Path-> Bool
 hasEnd path = isEnd(last path)
 
+--Does the list of paths have a end?
 containsEnd::[Path]-> Bool
 containsEnd [] = False;
 containsEnd (h:t) = if(hasEnd h) then True else containsEnd t
 
+--Get the following nodes in new path objects
 possibleNext::Maze->Path->[Path]
 possibleNext maze [] = []
 possibleNext maze path = [path++[newNode]| newNode<-(getNextNodes maze (last path))]
 
+--Get a set of next paths that are larger then the specified number
 nextPath::Maze->[Path]->Int->[Path]
 nextPath maze [] numb = []
 nextPath maze paths numb = [newPaths|path<-paths, newPaths<-possibleNext maze path, length newPaths>=numb||hasEnd newPaths ]
 
+--Only take paths that have a end
 filterPaths::[Path]->[Path]
 filterPaths [] = []
 filterPaths paths = [enders|enders<-paths, hasEnd enders]
 
-
+--Finds possible paths, quit if one path reached a end.
 findPaths::Maze->[Path]->Int->[Path]
 findPaths maze [] numb = []
 findPaths maze paths numb = if containsEnd paths then filterPaths paths else findPaths maze (nextPath maze paths (numb)) (numb+1)
 
-solveMaze::Maze->[Path]
+--Solve a maze!
+solveMaze::Maze->Path
 solveMaze [] = []
-solveMaze maze = findPaths maze [[findStart maze]] 2
+solveMaze maze = last (findPaths maze [[findStart maze]] 2)
