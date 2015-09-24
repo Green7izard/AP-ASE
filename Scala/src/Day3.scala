fe47157847a658151741fde90f9484d583f3549d
Bas van Summeren <basvansummeren@hotmail.com>
2015-09-24 14:11:56 +0200

    Added the code from the book and made it compileable and runnable

--- Scala/src/Day3.scala
+++ Scala/src/Day3.scala
@@ -1,8 +1,60 @@
 /**
  * Created by Bas on 24-9-2015.
  */
+
+import scala.io._
+import scala.actors._
+import Actor._
+
+class PageSizeReader(urls: List[String]) {
+
+  def timeMethod(method: () => Unit) = {
+    val start = System.nanoTime
+    method()
+    val end = System.nanoTime
+    println("Method took " + (end - start) / 1000000000.0 + " seconds.")
+  }
+
+  def getPageSizeSequentially() = {
+    for (url <- urls) {
+      println("Size for " + url + ": "
+        + PageLoader.getPageSize(url))
+    }
+  }
+
+  def getPageSizeConcurrently() = {
+    val caller = self
+    for (url <- urls) {
+      actor {
+        caller !(url, PageLoader.getPageSize(url))
+      }
+    }
+    for (i <- 1 to urls.size) {
+      receive { case (url, size) => println("Size for "
+        + url +
+        ": "
+        + size)
+      }
+    }
+  }
+
+  object PageLoader {
+    def getPageSize(url: String) = Source.fromURL(url).mkString.length
+  }
+
+}
+
 object Day3 {
   def main(args: Array[String]): Unit = {
-    println ("Do nothing")
+    val urls = List("http://www.amazon.com/", "http://www.twitter.com/", "http://www.google.com/", "http://www.cnn.com/")
+    val reader = new PageSizeReader(urls)
+    println("Sequential run:")
+    reader.timeMethod {
+      reader.getPageSizeSequentially
+    }
+    println("Concurrent run")
+    reader.timeMethod {
+      reader.getPageSizeConcurrently
+    }
   }
 }
