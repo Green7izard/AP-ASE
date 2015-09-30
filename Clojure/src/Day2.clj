afaeccfc5b9b4f6ea56ba635254a32da7e1f1f03
Bas van Summeren <basvanummeren@hotmail.com>
2015-09-30 21:17:11 +0200

    Clojure Day2!

---
+++ Clojure/src/Day2.clj
@@ -0,0 +1,34 @@
+;; Clojure Day 2
+;; Bas van Summeren <BasvanSummeren@hotmail.com>
+(defmacro unless [test body else] (list 'if (list 'not test) body else))
+
+(println "Unless true :incorrect :correct becomes: " (unless true :incorrect :correct))
+(println "Unless false :correct :incorrect becomes: " (unless false :correct :incorrect))
+
+
+(println "\n\nDice protocol: ")
+
+(defprotocol Dice
+  (roll [d])
+  )
+
+::https://clojuredocs.org/clojure.core/rand-int
+(defrecord DefaultDice [] Dice
+     (roll[d](+ (rand-int 6) 1))
+     (toString [this] (str "[" (this) "]" ))
+     )
+
+(println "Creating a default dice (1 to 6)")
+(def aDice (DefaultDice.))
+;;https://clojuredocs.org/clojure.core/dotimes
+(dotimes [n 10] (println "Rolling a dice: " (roll aDice) ))
+
+(println "\nCreating a D20 (1 to 20)")
+(defrecord VariableDice [number] Dice
+  (roll[d](+ (rand-int number) 1))
+  )
+(def d20 (VariableDice. 20))
+(dotimes [n 5] (println "Rolling the D20: " (roll d20) ))
+(println "Creating a D5000 (1 to 5000)")
+(def d5000 (VariableDice. 5000))
+(dotimes [n 5] (println "Rolling the D5000: " (roll d5000) ))
