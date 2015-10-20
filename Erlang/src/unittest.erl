99d5e41e4ee1978768355a98b8d13adefa991107
Bas van Summeren <basvanummeren@hotmail.com>
2015-10-20 17:47:35 +0200

    Erlang unit test

---
+++ Erlang/src/unittest.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author Bas van Summeren
+%%% @doc
+%%%   a previeuw of a unit test of erlang
+%%% @end
+%%% Created : 20. okt 2015 17:41
+%%%-------------------------------------------------------------------
+-module(unittest).
+-author(Bas van Summeren").
+
+-include_lib("eunit/include/eunit.hrl").
+
+%http://www.erlang.org/doc/apps/eunit/chapter.html
+count_nothing_test() ->?assert(day1:countWords("") =:= 0).
+count_two_test() -> ?assert(day1:countWords("Hello World!")=:=2).
